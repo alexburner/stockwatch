c8f77b95115b3e1fbba67ee00b3ea5ba7e353c93
Alex Burner <alex.burner@gmail.com>
2018-02-18 21:41:47 -0800

    First pass styling echarts

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,100 @@
 import 'babel-polyfill'
+import format from 'date-fns/format'
 import ReactEcharts from 'echarts-for-react'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { getCharts, getSymbols, TimeRange } from 'src/data/api'
+import { ChartDatum, getCharts, getSymbols, TimeRange } from 'src/data/api'
 
 const symbols = ['VTI', 'VXUS', 'VNQ', 'BND']
+
+const getDotHtml = (color: string): string => `
+  <span style="
+      display:inline-block;
+      margin-right:5px;
+      border-radius:10px;
+      width:10px;
+      height:10px;
+      background-color:${color};
+  "></span>
+`
 
 const option: echarts.EChartOption = {
   xAxis: {
     type: 'time',
+    show: false,
   },
   yAxis: {
     type: 'value',
     scale: true,
-  },
-  tooltip: {
-    trigger: 'axis',
+    show: false,
     axisPointer: {
-      type: 'cross',
+      type: 'line',
+      lineStyle: {
+        opacity: 0.5,
+        type: 'dotted',
+      },
       label: {
         backgroundColor: 'rgba(0,0,0,0.5)',
         borderColor: 'none',
         shadowColor: 'none',
+        formatter({ seriesData, value }: { seriesData: any; value: number }) {
+          const date = new Date(value)
+          if (date.getFullYear() < 1995) return Number(value).toFixed(2)
+          else return format(date, 'MMM Do YYYY')
+        },
+      },
+    },
+  },
+  tooltip: {
+    trigger: 'axis',
+    formatter(params: any[]) {
+      return params
+        .map(p => {
+          const label = p.seriesName
+          const value = p.value[p.seriesId] // XXX codename tribeca
+          const dot = getDotHtml(p.color)
+          return `${dot} ${label}: ${value}`
+        })
+        .join('<br/>')
+    },
+    axisPointer: {
+      type: 'cross',
+      lineStyle: {
+        opacity: 0.5,
+      },
+      label: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        borderColor: 'none',
+        shadowColor: 'none',
+        formatter({ seriesData, value }: { seriesData: any; value: number }) {
+          const date = new Date(value)
+          if (date.getFullYear() < 1995) return Number(value).toFixed(2)
+          else return format(date, 'MMM D YYYY')
+        },
       },
     },
     backgroundColor: 'rgba(0,0,0,0.5)',
   },
   series: [
     {
+      id: 'high', // XXX codename tribeca
+      name: 'High',
       type: 'line',
+      showSymbol: false,
       encode: {
         x: 'date',
-        y: 'close',
+        y: 'high',
+      },
+    },
+    {
+      id: 'low', // XXX codename tribeca
+      name: 'Low',
+      type: 'line',
+      showSymbol: false,
+      encode: {
+        x: 'date',
+        y: 'low',
       },
     },
   ],
